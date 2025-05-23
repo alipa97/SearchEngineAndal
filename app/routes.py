0d98d2d050794a7e37c93478dc138a7cd8ebd66f
narenridha <narendraridhabaihaqi@upi.edu>
2025-05-23 19:40:38 +0700

    bfs&dfs

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 from app.crawler.crawler import crawl
-from app.search.engine import search_keyword
+from app.search.engine import search_keyword_bfs, search_keyword_dfs
 
 main = Blueprint('main', __name__)
 
@@ -11,7 +11,11 @@ def index():
 @main.route('/search', methods=['POST'])
 def search():
     keyword = request.form['keyword']
-    results = search_keyword(keyword)
+    algo = request.form.get('algo', 'bfs')
+    if algo == 'dfs':
+        results = search_keyword_dfs(keyword)
+    else:
+        results = search_keyword_bfs(keyword)
     return render_template('results.html', keyword=keyword, results=results)
 
 @main.route('/route')
--- app/search/engine.py
+++ app/search/engine.py
@@ -11,3 +11,39 @@ def search_keyword(keyword):
                 "path": page["path"]
             })
     return matched_urls
+
+def search_keyword_bfs(keyword):
+    keyword = keyword.lower()
+    matched_urls = []
+    for page in get_pages():
+        if keyword in page["text"].lower():
+            matched_urls.append({
+                "url": page["url"],
+                "title": page["title"],
+                "path": page["path"]
+            })
+    return matched_urls
+
+def search_keyword_dfs(keyword):
+    keyword = keyword.lower()
+    matched_urls = []
+    pages = get_pages()
+    visited = set()
+    def dfs(idx):
+        if idx in visited:
+            return
+        visited.add(idx)
+        page = pages[idx]
+        if keyword in page["text"].lower():
+            matched_urls.append({
+                "url": page["url"],
+                "title": page["title"],
+                "path": page["path"]
+            })
+        # DFS ke halaman berikutnya (berdasarkan urutan simpan path)
+        for i, p in enumerate(pages):
+            if p["path"][:-1] == page["path"] and i not in visited:
+                dfs(i)
+    for i in range(len(pages)):
+        dfs(i)
+    return matched_urls
